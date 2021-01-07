3fb116bb80fa3f87d72a9036868ee1e793323572
Muhammad Stracey <strmuh001@gmail.com>
2021-01-07 15:23:11 +0200

    Methods for Density and threhold calculations added

--- Density_calc.py
+++ Density_calc.py
@@ -26,9 +26,35 @@ class Determinedensity():
 
     # Create method for reading image files
     def loadImages(self):
+        imgrays = {}
         for i, image in enumerate(self.fileList()): # Loop over images
             # Create variable for image
             im = cv2.imread(str(image))
             # Convert image Grayscale
-            imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-            print(im)
+            imgrays[image] = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        return imgrays
+
+    def densities(self, threshold):
+        Hist = {}
+        Densities = {}
+        for name, imgray in self.loadImages().items():
+            counts, bins = np.histogram(imgray, range(256))
+            # Store Histogram data in Dictionary
+            Hist[name] = [[counts], [bins]]
+            Total_Pixels = sum(counts)
+            voids = 0
+            for a in range(1, int(threshold)):
+                voids += counts[a]
+            Density = 100 - voids / Total_Pixels * 100
+            Densities[name] = Density
+        return Densities
+
+    def autothreshold(self):
+        autoThresh = {}
+        autoImage = {}
+        for name, imgray in self.loadImages().items():
+            th3, ret3 = cv2.threshold(imgray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_TRIANGLE)
+            autoThresh[name]= th3
+            autoImage[name] = ret3
+            autoDensities = self.densities(th3)
+        return autoDensities
